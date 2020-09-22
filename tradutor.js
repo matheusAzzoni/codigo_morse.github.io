3e4d96b042ef447919410610da4306663ed697c7
Matheus dos Santos Azzoni <matheusazzoni@gmail.com>
2020-09-22 18:31:21 -0300

    update

--- tradutor.js
+++ tradutor.js
@@ -18,29 +18,16 @@ function hide(elem){
 }
 window.onload=function(){
     "use strict";
-    var  encodeEnter=document.getElementById("encodeEnter"),
-    decodeEnter=document.getElementById("decodeEnter"),
-    decBack=document.getElementById("decBack"),
+    var
     decBtn=document.getElementById("decBtn"),
     encBtn=document.getElementById("encBtn"),
     encText=document.getElementById("encText"),
     decCode=document.getElementById("decCode"),
-    mainDiv=document.getElementById("mainDiv"),
-    encodeDiv=document.getElementById("encodeDiv"),
-    encBack=document.getElementById("encBack"),
     encResult=document.getElementById("encResult"),
     decResult=document.getElementById("decResult"),
     letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"," ","\n"],
         symbols=[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..","-----",".----","..---","...--","....-",".....","-....","--...","---..","----.","^","\n"];
     /// encode
-    encodeEnter.onclick=function(){
-        hide(mainDiv);
-        fadeIn(encodeDiv);
-    }
-    encBack.onclick=function(){
-        hide(encodeDiv);
-        fadeIn(mainDiv);
-    }
     encBtn.onclick=function(){
        var txt=encText.value.trim().toUpperCase().split(""),
        code="";
@@ -49,17 +36,17 @@ window.onload=function(){
        }
        encResult.innerText=code;
        fadeIn(encResult);
+       fadeIn(encBtnRes);
     }
+    encBtnRes.onclick=function(){
+        hide(encResult);
+        hide(encBtnRes);
+    }
+
+
     /// decode
-    decodeEnter.onclick=function(){
-        hide(mainDiv);
-        fadeIn(decodeDiv);
-    }
-    decBack.onclick=function(){
-        hide(decodeDiv);
-        fadeIn(mainDiv);
-    }
     decBtn.onclick=function(){
+        
         var code=decCode.value.trim().replace(/_|¯|—|–/g,"-").split(" "),
         txt="";
         
@@ -68,6 +55,29 @@ window.onload=function(){
         }
         decResult.innerText=txt.replace("undefined","");
         fadeIn(decResult);
+        fadeIn(decBtnRes)
+    }
+    decBtnRes.onclick=function(){
+        hide(decResult);
+        hide(decBtnRes);
     }
     
 }
+function GoToHomePage()
+{
+    window.location = 'index.html';
+}
+function GoToTradutor()
+{
+    window.location = '#tradutor';
+}
+function GoToTop()
+{
+    window.location = '#b';
+}
+$(document).ready(function() {
+	$('#subir').click(function(){
+		$('html, body').animate({scrollTop:0}, 'slow');
+		return false;
+	});
+});
